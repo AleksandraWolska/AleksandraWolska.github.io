a2c6bc63ce15189f10d1acbacd94985197f3cbc7
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2022-04-03 16:10:12 +0200

    added project section

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
- 
+toggleHamburgerMenu = (flag = false) => {
+    let menu = document.querySelector("#menu")
+    if (flag || menu.className === "menu") {
+        menu.className += " opened"
+    } else {
+        menu.className = "menu"
+    }
+}
+
+
+
+
